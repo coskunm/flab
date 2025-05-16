fb6691aac0ae1ef76dac258108c2e1619b4a0742
coskunm <57809681+coskunm@users.noreply.github.com>
2025-05-16 22:20:55 +0300

    assets/index.ts dosyasını ekle

---
+++ src/assets/index.ts
@@ -0,0 +1,27 @@
+// Import broker logos
+import oliveMarketsLogo from './images/brokers/olive-markets-560x350.png';
+import avaTradeLogo from './images/brokers/avatrade-logo-350x350.png';
+import fxProLogo from './images/brokers/Fx-Pro-1-350x350.png';
+import artsMarketsLogo from './images/brokers/artsmrkts-logo-350x350.png';
+import exnessLogo from './images/brokers/exness-560x350.png';
+import fxtmLogo from './images/brokers/fxtm-560x350.png';
+import hotForexLogo from './images/brokers/hotforex-303x150-1-707x350.png';
+import pepperstoneLogo from './images/brokers/pepperstone-560x350.png';
+import tickmillLogo from './images/brokers/tickmill-guvenilir-mi-1-1696x350.png';
+import xmForexLogo from './images/brokers/xm-forex-560x350.png';
+
+// Export broker logos
+export const brokerLogos = {
+  oliveMarkets: oliveMarketsLogo,
+  avaTrade: avaTradeLogo,
+  fxPro: fxProLogo,
+  artsMarkets: artsMarketsLogo,
+  exness: exnessLogo,
+  fxtm: fxtmLogo,
+  hotForex: hotForexLogo,
+  pepperstone: pepperstoneLogo,
+  tickmill: tickmillLogo,
+  xmForex: xmForexLogo
+};
+
+export default brokerLogos;
